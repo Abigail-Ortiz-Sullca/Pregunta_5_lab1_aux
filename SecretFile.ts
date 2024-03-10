55ab6e54501178de543c560f39bdd711ab6131bf
OrtizAbigail <amoscma469@gmail.com>
2024-03-09 22:32:58 -0400

    Añadido el archivo SecretFile.ts

---
+++ SecretFile.ts
@@ -0,0 +1,11 @@
+(() => {
+    class SecretFile { 
+      secretMessage() {
+        console.log("Shhhh...");	
+      }
+    }
+    const secretFile = new SecretFile();
+    secretFile.secretMessage();
+  })();
+  
+  
